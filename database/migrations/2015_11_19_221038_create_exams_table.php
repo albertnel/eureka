766ad3beff19e1c67d215b4fcb95f8a4848c08a9
Albert Nel <albert@spiraleye.com>
2015-11-19 23:33:46 +0200

    Adds exams
    * Model, controller and request.
    * Migration with fields as per old Full Marks.

---
+++ database/migrations/2015_11_19_221038_create_exams_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exams', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->date('start_date');
+            $table->time('start_time');
+            $table->date('end_date');
+            $table->time('end_time');
+            $table->time('time_limit');
+            $table->boolean('feedback');
+            $table->time('feedback_time_limit');
+            $table->boolean('feedback_comments');
+            $table->boolean('display_results');
+            $table->boolean('randomize_question');
+            $table->boolean('force_complete');
+            $table->boolean('allow_navigation');
+            $table->boolean('allow_answer_editing');
+            $table->boolean('allow_logout');
+            $table->unsignedTinyInteger('default_allowed_attempts');
+            $table->boolean('is_survey');
+            $table->string('password', 60);
+            $table->text('info');
+            $table->boolean('display_student_name');
+            $table->boolean('display_exam_name');
+            $table->boolean('display_exam_date');
+            $table->boolean('display_exam_time');
+            $table->boolean('display_marks_total');
+            $table->boolean('display_marks_per_library');
+            $table->boolean('display_marks_percentage');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('exams');
+    }
+}
