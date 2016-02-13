b247c650fb0e8aeaf1df28a872073c0d9adc111c
Albert Nel <albert@spiraleye.com>
2016-02-13 12:46:34 +0200

    Adds one-to-many save on Question

--- app/Question.php
+++ app/Question.php
@@ -6,6 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    protected $fillable = [
+        'question_code',
+        'question',
+        'marks',
+        'marks_negative',
+        'difficulty',
+        'case_sensitive',
+        'randomize_options',
+        'num_options_to_display',
+        'active'
+    ];
+
+    public function saveQuestion($params, $library)
+    {
+        $question = $library->questions()->create($params);
+        return $question;
+    }
+
     public function libraries()
     {
     	return $this->belongsTo('App\Library');
--- app/QuestionMcqAnswer.php
+++ app/QuestionMcqAnswer.php
@@ -2,10 +2,16 @@
 
 namespace App;
 
+use App\Question;
 use Illuminate\Database\Eloquent\Model;
 
 class QuestionMcqAnswer extends Model
 {
+    public function create($params, $question)
+    {
+
+    }
+
     public function getQuestionData()
     {
     	return json_decode($this->question_data, true);
